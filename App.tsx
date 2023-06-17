053cff5febfd3a086f6aa2c5c70aedfbef15a434
Jose-WDG <neto.silvacem@gmail.com>
2023-06-17 19:37:38 -0300

    colocando informações e alterando estilos.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import type { PropsWithChildren } from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -19,16 +13,19 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
+const Header: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('./assets/foto-jose.jpg')} />
+    </View>
+  );
+};
 function Section({ children, title }: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -37,7 +34,7 @@ function Section({ children, title }: SectionProps): JSX.Element {
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
+            color: '#ed145b',
           },
         ]}>
         {title}
@@ -46,7 +43,7 @@ function Section({ children, title }: SectionProps): JSX.Element {
         style={[
           styles.sectionDescription,
           {
-            color: isDarkMode ? Colors.light : Colors.dark,
+            color: Colors.light
           },
         ]}>
         {children}
@@ -57,48 +54,44 @@ function Section({ children, title }: SectionProps): JSX.Element {
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
+  //const imageUrl = require('./assets/foto-jose.jpg');
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.background}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Header />
+          <Section title="José Ramalho da silva neto">
+            <Text style={styles.highlight}>Desenvolvedor mobile </Text>
+            altamente experiente, com uma sólida formação em análise e desenvolvimento de sistemas e 3 anos de
+            experiência em desenvolvimento de aplicativos móveis. Meu foco principal é trabalhar com grandes
+            corporações financeiras, criando aplicativos inovadores e funcionais que atendam às necessidades
+            específicas de seus clientes. Com um bom conhecimento em tecnologias móveis, sou capaz de criar
+            aplicativos móveis robustos e escaláveis. Conhecimentos em iOS e Android, estou sempre procurando
+            maneiras de usar as tecnologias mais recentes para aprimorar os projetos. Excelente comunicação e
+            trabalho em estreita colaboração com minha equipe para garantir máxima qualidade. Comprometido em
+            entregar projetos dentro do prazo. Quando não estou trabalhando, gosto de jogar video game e adoro
+            viajar para novos lugares. Estou sempre procurando maneiras de crescer na minha carreira como desenvolvedor,
+            buscando aprimorar minhas habilidades e aprender novas tecnologias para fazer projetos de qualidade.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
-    </SafeAreaView>
+
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
+    marginBottom: 32,
     paddingHorizontal: 24,
   },
   sectionTitle: {
@@ -112,6 +105,19 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 200,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 150,
+  },
+  background: {
+    backgroundColor: '#151819'
   },
 });
 
